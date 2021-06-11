a3c7c18d42f80027b484d27721e03375da8297d6
konstantin.udilovich <konstantin.udilovich@wsp.com>
2021-06-10 20:20:24 -0400

    Initial Vue.js project commit

---
+++ web-analysis-viewer/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template >
+  <div class="home" style="background-color: red">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <VtkView msg="VtkViewer" style="height: 500px"/>
+  </div>
+</template>
+
+<script>
+import { ref, unref, onMounted, onBeforeUnmount, watchEffect } from 'vue';
+// @ is an alias to /src
+import VtkView from '@/components/VtkView.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    VtkView
+  }
+}
+</script>
